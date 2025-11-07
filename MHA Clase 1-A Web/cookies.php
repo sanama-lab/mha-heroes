020b5bf057c05573325a0f29a20eb9420a5ea0fb
sanama-lab <omori14102008@gmail.com>
2025-11-07 19:12:07 -0300

    index2

--- MHA Clase 1-A Web/cookies.php
+++ MHA Clase 1-A Web/cookies.php
@@ -1,9 +1,9 @@
 <?php
     // Este archivo es llamado por el formulario usando el método POST.
     // Asi que, vamos a recibir los datos desde el arreglo de ese método
-    $nombre = $_POST['nombre'];
+    $nombre = $_POST['usuario'];
     $email = $_POST['email'];
-    $contraseña = $_POST['contraseña'];
+    $contraseña = $_POST['password'];
 
     // Vamos a guardar estos datos en una cookie que dure 1 minuto (solo es una prueba)
     setcookie("nombre", $nombre, time() + 60);
